f09d3ae08ee25dcdf974d472dc1d35ebbcef7f13
Luiz Guilherme Anduze <l.g.anduze@gmail.com>
2022-07-18 17:06:04 -0300

    Backend 1 challenge created

---
+++ backend/src/test/java/com/lgdev/cruddesafio/CrudDesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lgdev.cruddesafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudDesafioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
